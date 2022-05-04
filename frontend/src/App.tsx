6708595c2a4eee15c37729121bbd0adebdb87b5c
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-05-04 23:41:04 +0530

    Added ability to add an audio file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import WaveSurferPlayer from "./components/WaveSurfer/WaveSurfer";
 import NavBar from "./components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
+ const [audioFile, setAudioFile] = useState<File | null>(null);
+
  return (
   <div className="min-h-screen w-11/12 mx-auto">
    <NavBar />
@@ -10,7 +13,7 @@ function App() {
      <h2>Check Sarcasm</h2>
      <h3>1. Record yourself saying something sarcastic</h3>
     </div>
-    <WaveSurferPlayer />
+    <WaveSurferPlayer onFileChange={(file) => setAudioFile(file)} />
    </section>
    <section></section>
   </div>
--- frontend/src/components/WaveSurfer/WaveSurfer.tsx
+++ frontend/src/components/WaveSurfer/WaveSurfer.tsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useRef, useState } from "react";
-import { BsFillPlayFill, BsFillStopFill } from "react-icons/bs";
+import { useEffect, useRef, useState } from "react";
+import { BsFillPlayFill, BsFillStopFill, BsUpload } from "react-icons/bs";
 import WaveSurfer from "wavesurfer.js";
 import Button from "../Button/Button";
 
-interface Props {}
+interface Props {
+ onFileChange?: (file: File) => void;
+}
 
-const WaveSurferPlayer = ({}: Props) => {
+const WaveSurferPlayer = ({ onFileChange }: Props) => {
  const waveSurfer = useRef<null | WaveSurfer>(null);
+ const fileInput = useRef<null | HTMLInputElement>(null);
+
  const [isWaveSurferPlaying, setIsWaveSurferPlaying] = useState(false);
  const [waveSurferReady, setWaveSurferReady] = useState(false);
+ const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
  useEffect(() => {
   function handleReady() {
-   waveSurfer.current?.fireEvent("ready");
    setWaveSurferReady(true);
+  }
+
+  function handleOnFinish() {
+   setIsWaveSurferPlaying(false);
+   waveSurfer.current && waveSurfer.current.seekTo(0);
   }
 
   if (!waveSurfer.current) {
    waveSurfer.current = WaveSurfer.create({
     container: "#wavesurfer",
-    waveColor: "#f0f0f0",
-    progressColor: "#394b5b",
+    waveColor: "#0f766d",
+    progressColor: "#14b8a5",
     backend: "MediaElement",
     loopSelection: false,
     scrollParent: true,
@@ -28,9 +37,12 @@ const WaveSurferPlayer = ({}: Props) => {
     responsive: true,
     height: 320
    });
+  }
 
+  if (waveSurfer.current) {
    waveSurfer.current.load(`${process.env.PUBLIC_URL}/1_60.wav`);
    waveSurfer.current.on("ready", handleReady);
+   waveSurfer.current.on("finish", handleOnFinish);
    waveSurfer.current.util.preventClick();
    waveSurfer.current.setVolume(1);
   }
@@ -47,15 +59,38 @@ const WaveSurferPlayer = ({}: Props) => {
   }
  }
 
+ async function handleAudioFileChange(event: any) {
+  const [fileUploaded]: File[] = event.target.files;
+  if (onFileChange) {
+   setSelectedFile(fileUploaded);
+   onFileChange(fileUploaded);
+  }
+ }
+
  return (
-  <div id="wavesurfer" className="bg-slate-900 relative rounded-xl h-80">
+  <div id="wavesurfer" className="bg-teal-900 relative rounded-xl h-80">
    <Button
     onMouseDown={handleWavesurferPlayStateChange}
-    className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hover:cursor-pointer z-10 gap-2"
+    className="absolute left-1/2 top-1/2 translate-x-1/4 -translate-y-1/2 hover:cursor-pointer z-10 gap-2"
     hasShadow
    >
     {isWaveSurferPlaying ? <BsFillStopFill /> : <BsFillPlayFill />}
     {isWaveSurferPlaying ? "Pause" : "Play"}
+   </Button>
+   <input
+    ref={fileInput}
+    type="file"
+    className="hidden"
+    onChange={handleAudioFileChange}
+    accept=".wav,.mp3,.mp4"
+   />
+   <Button
+    onMouseDown={() => fileInput.current?.click()}
+    className="absolute right-1/2 top-1/2 -translate-x-1/4 -translate-y-1/2 hover:cursor-pointer z-10 gap-2 max-w-prose overflow-ellipsis"
+    hasShadow
+   >
+    <BsUpload />
+    {selectedFile ? selectedFile.name : "Upload"}
    </Button>
   </div>
  );
