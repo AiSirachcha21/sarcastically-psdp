7c3ad2384b59624bb3b32c9ad456dedca3327501
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-05-04 20:39:26 +0530

    Added Tailwind,WaveSurfer and other related packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+ content: ["./src/**/*.{js,jsx,ts,tsx}"],
+ theme: {
+  extend: {
+   fontFamily: {
+    poppins: ["Poppins", "sans-serif"],
+    pacifico: ["Pacifico", "cursive"]
+   }
+  }
+ },
+ plugins: []
+};
