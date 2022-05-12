1d00bb8f0a175d791ad3bbf3052a514af98bff12
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-05-13 04:58:43 +0530

    Fixed navbar padding in tablet and larger screens

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,12 +3,12 @@ import NavLink from "./NavLink";
 
 const NavBar = () => {
  return (
-  <nav className={clsx("hidden md:flex", "justify-between", "py-8")}>
-   <NavLink href="#">Check Sarcasm</NavLink>
+  <nav className={clsx("hidden md:flex", "justify-between", "py-8 px-4")}>
+   <NavLink href="/">Check Sarcasm</NavLink>
    <NavLink href="/" className="font-pacifico text-4xl font-normal">
     Sarcastically
    </NavLink>
-   <NavLink href="#">About Sarcastically</NavLink>
+   <NavLink href="/about">About Sarcastically</NavLink>
   </nav>
  );
 };
